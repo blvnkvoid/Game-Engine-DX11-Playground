ec244ef303bba423fdee84646e5c88772657a84e
blvnkvoid <marcinarasik@gmail.com>
2026-06-26 11:30:27 +0200

    Tracks update

---
+++ Project1/Tracks/TrackTable.cpp
@@ -0,0 +1,21 @@
+#include "TrackTable.h"
+
+const std::vector<TrackEntry> g_TrackTable =
+{
+    { TrackSelection::AutumnRing,     "Tracks/AutumnRing/AutumnRing.fbx" },
+    { TrackSelection::ElCapitan,      "Tracks/ElCapitan/ElCapitan.fbx" },
+    { TrackSelection::Spa,            "Tracks/SpaFrancorshamps/Spa3.fbx" },
+    { TrackSelection::GrandValley,    "Tracks/GrandValleySpeedway/GrandValley.fbx" },
+    { TrackSelection::TrialMountain,  "Tracks/TrialMountain/TrialMountain.fbx" },
+    { TrackSelection::HighSpeedRing,  "Tracks/HighSpeedRing/HighSpeedRing.fbx" },
+    { TrackSelection::MidfieldRaceway,"Tracks/MidfieldRaceway/MidfieldRaceway.fbx" },
+    { TrackSelection::TestCourse,     "Tracks/TestCourse/TestCourse.fbx" },
+    { TrackSelection::RouteX,         "Tracks/RouteX/RouteX.fbx" },
+    { TrackSelection::RoadAtlanta,    "Tracks/RoadAtlanta/RoadAtlanta2.fbx" },
+    { TrackSelection::Nordschleife,   "Tracks/Nurburgring/Nordschleife2.fbx" },
+    { TrackSelection::BeginnerCourse,   "Tracks/BeginnerCourse/BeginnerCourse.fbx" },
+    { TrackSelection::Motorland,   "Tracks/Motorland/Motorland.fbx" },
+    { TrackSelection::Tsukuba,   "Tracks/Tsukuba/Tsukuba.fbx" },
+    { TrackSelection::LeMans,   "Tracks/LeMans/LeMans.fbx" },
+    { TrackSelection::DeepForest,   "Tracks/DeepForestRaceway/DeepForestRaceway.fbx" }
+};
---
+++ Project1/Tracks/TrackTable.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "../SharedTypes.h"
+
+struct TrackEntry
+{
+    TrackSelection selection;
+    std::string path;
+};
+
+extern const std::vector<TrackEntry> g_TrackTable;
